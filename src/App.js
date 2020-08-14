8001920ac2df24521094e25c8dd6ac38ca8a9304
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-08-13 21:53:45 -0500

    fixed the day delay bug

--- src/App.js
+++ src/App.js
@@ -5,16 +5,39 @@ import Footer from "./Footer";
 import axios from "axios";
 // import "./App.css";
 
+// const day = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+// const newDay = day.setDate(day.getDate() - 1);
+// newDay.toString();
+// // day.setDate(day.getDate() - 1);
+
+// console.log(newDay);
+
+// const today = new Date().format('m-d-Y');
+// console.log(today);
+
+// const day = Date.now();
+// day.toJSON().slice(0, 10).replace(/-/g, "/");
+// day.toString();
+// console.log(day);
+
+var date = new Date(Date.now());
+var dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
+                    .toISOString()
+                    .split("T")[0];
+
+console.log(dateString);
 function App() {
   const [apods, setApod] = useState([]);
+  // const api_key = "CtUPAONsTf6UnI5ZLRbJkgTogRdByfCgR6W6TGf2#";
 
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=CtUPAONsTf6UnI5ZLRbJkgTogRdByfCgR6W6TGf2#")
-    .then(response => {
-      setApod(response.data);
-      console.log(response);
-    })
-    .catch(error => console.log("Something went wrong: ", error));
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dateString}`)
+      .then((response) => {
+        setApod(response.data);
+        console.log(response);
+      })
+      .catch((error) => console.log("Something went wrong: ", error));
   }, []);
 
   return (
